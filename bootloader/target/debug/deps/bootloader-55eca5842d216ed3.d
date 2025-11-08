62166c2c34f39e43f68f0bd96db3ba7ad600676f
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-11-08 14:47:16 -0800

    Delete bootloader/target directory

--- bootloader/target/debug/deps/bootloader-55eca5842d216ed3.d
+++
@@ -1,9 +0,0 @@
-/home/had/Developer/github_repos/OxieContri/bootloader/target/debug/deps/bootloader-55eca5842d216ed3.d: src/main.rs Cargo.toml
-
-/home/had/Developer/github_repos/OxieContri/bootloader/target/debug/deps/libbootloader-55eca5842d216ed3.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
--- bootloader/target/x86_64-unknown-uefi/debug/bootloader.d
+++
@@ -1,1 +0,0 @@
-/home/had/Developer/github_repos/OpenContriOS/bootloader/target/x86_64-unknown-uefi/debug/bootloader.efi: /home/had/Developer/github_repos/OpenContriOS/bootloader/src/main.rs
