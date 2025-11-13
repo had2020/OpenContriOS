18a5db17b3a6d36f2c6db334391935d445c16c10
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-11-13 00:28:27 -0800

    Update main.rs

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,17 +3,28 @@
 
 use core::time::Duration;
 use log::info;
+use uefi::Identify;
 use uefi::boot::exit_boot_services;
 use uefi::boot::memory_map;
 use uefi::prelude::*;
 //use uefi::system USE later
 use core::{mem, ptr};
 use uefi::boot;
-use uefi::table;
+use uefi::proto::console::gop::GraphicsOutput;
+use uefi::proto::console::gop::ModeInfo;
 
 #[repr(C)]
 pub struct BootInfo {
-    pub framebuffer: FramebufferInfo,
+    // frame_buffer
+    pub frame_buffer_ptr: *mut u8, // virtual addr after map
+    pub frame_buffer_base: u64,    // phys addr
+    pub frame_buffer_size: u64,    // total bytes
+    pub frame_buffer_resolution_x: u64,
+    pub frame_buffer_resolution_y: u64,
+    pub frame_buffer_pixel_format: u8, // UEFI magics PixelFormat
+    pub frame_buffer_stride: usize,
+
+    // Conventional Memory
     pub memory_map_ptr: u64, // later
     pub memory_map_len: usize,
     pub rsdp_addr: u64,
@@ -26,8 +37,10 @@ fn efi_main() -> Status {
     uefi::helpers::init().unwrap();
 
     info!("Initiating OpenContriOS Bootloader.");
-    info!("uefi::helpers::init success! (Logging, and Allocator) Now Online!");
-    // now we have uefi::allocator::Allocator!
+    info!("uefi::helpers::init success! UEFI (Logging, and Allocator) Now Online!");
+
+    let gop_mode_info: ModeInfo = GraphicsOutput::current_mode_info(&self);
+    let frame_buffer_info = GraphicsOutput::frame_buffer(&mut self);
 
     /* TODO
     // memory map
