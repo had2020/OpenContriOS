9c489d57d3131625cbc39f32277f3777879575e1
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-11-14 14:27:01 -0800

    Update main.rs

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -8,7 +8,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+#[no_mangle]
+pub extern "C" fn kernel_main(boot_info: *const ()) -> ! {
     loop {}
 }
