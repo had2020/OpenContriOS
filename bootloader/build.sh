e3d40b01dbb121c8821c6f8b124e4ff860519fd2
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-11-14 12:24:43 -0800

    moving towards coreboot away from UEFI

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -1,8 +1,5 @@
 # Only tested on Arch Linux (WIP) build shell script.
 
-cargo build --target x86_64-unknown-uefi
-
-truncate -s 64M uefi.img
-mkfs.vfat -n UEFI uefi.img
-mmd   -i uefi.img ::/EFI ::/EFI/BOOT
-mcopy -i uefi.img target/x86_64-unknown-uefi/debug/bootloader.efi ::/EFI/BOOT/BOOTX64.EFI
+#cargo build --target i686-unknown-none
+#cargo build --target x86-unknown-bare_metal.json
+cargo build --target i386-unknown-none.json
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,86 +1,25 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
-use core::ptr::NonNull;
-use core::time::Duration;
-use log::info;
-use uefi::Identify;
-use uefi::boot::OpenProtocolParams;
-use uefi::boot::exit_boot_services;
-use uefi::boot::memory_map;
-use uefi::prelude::*;
-use uefi::proto::console::gop::FrameBuffer;
-use uefi::table::system_table_raw;
-//use uefi::system USE later
-use core::{mem, ptr};
-use uefi::Handle;
-use uefi::boot;
-use uefi::proto::console::gop::GraphicsOutput;
-use uefi::proto::console::gop::ModeInfo;
-use uefi::proto::device_path::text::{AllowShortcuts, DevicePathToText, DisplayOnly};
-use uefi::proto::unsafe_protocol;
+#![feature(lang_items)]
 
-#[repr(C)]
-pub struct BootInfo {
-    // frame_buffer
-    pub frame_buffer_ptr: *mut u8, // virtual addr after map
-    pub frame_buffer_base: u64,    // phys addr
-    pub frame_buffer_size: u64,    // total bytes
-    pub frame_buffer_resolution_x: u64,
-    pub frame_buffer_resolution_y: u64,
-    pub frame_buffer_pixel_format: u8, // UEFI magics PixelFormat
-    pub frame_buffer_stride: usize,
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
 
-    // Conventional Memory
-    pub memory_map_ptr: u64, // later
-    pub memory_map_len: usize,
-    pub rsdp_addr: u64,
-    pub kernel_phys_start: u64,
-    pub kernel_phys_end: u64,
+//use core::panic::PanicInfo;
+//use core::sync::atomic;
+//use core::sync::atomic::Ordering;
+
+#[unsafe(no_mangle)]
+fn _start() -> ! {
+    loop {}
 }
 
-#[entry]
-fn efi_main() -> Status {
-    uefi::helpers::init().unwrap();
-
-    info!("Initiating OpenContriOS Bootloader.");
-    info!("uefi::helpers::init success! UEFI (Logging, and Allocator) Now Online!");
-
-    /*
-    unsafe {
-        boot::open_protocol(
-            OpenProtocolParams {
-                handle: Handle::new(),
-                agent: Handle::new(ptr),
-                controller: OP,
-            },
-            attributes,
-        )
+#[inline(never)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+       // atomic::compiler_fence(Ordering::SeqCst);
     }
-    */
-
-    //let gop: &mut GraphicsOutput = &mut GraphicsOutput;
-
-    let st = system_table_raw().unwrap().as_ptr();
-
-    //unsafe {
-    //let stdout_handle: Handle = Handle((*st).stdout_handle);
-    //let stdin_handle: Handle = Handle::from_ptr(*st).unwrap().stdout_handle;
-    //}
-
-    //boot::open_protocol(params, attributes);
-
-    //let gop_mode_info: ModeInfo = GraphicsOutput::current_mode_info();
-    //let frame_buffer_info = GraphicsOutput::frame_buffer();
-
-    /* TODO
-    // memory map
-    let mem_map = memory_map();
-
-    unsafe {
-        exit_boot_services(custom_memory_type);
-    }
-    */
-
-    Status::SUCCESS
 }
+
