bb7a1704d62baf4b3fea496e797d0bc04f6ad696
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-11-15 20:40:21 -0800

    adding limine into the kernel for a start

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,14 +1,69 @@
-#![no_std] 
+#![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::arch::asm;
 
-#[unsafe(no_mangle)] 
-pub extern "C" fn _start() -> ! {
-    loop {}
+use limine::BaseRevision;
+use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
+
+#[used]
+// The .requests section allows limine to find the requests faster and more safely.
+#[unsafe(link_section = ".requests")]
+static BASE_REVISION: BaseRevision = BaseRevision::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests_start_marker")]
+static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
+#[used]
+#[unsafe(link_section = ".requests_end_marker")]
+static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn kmain() -> ! {
+    // All limine requests must also be referenced in a called function, otherwise they may be
+    // removed by the linker.
+    assert!(BASE_REVISION.is_supported());
+
+    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
+        if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
+            for i in 0..100_u64 {
+                // Calculate the pixel offset using the framebuffer information we obtained above.
+                // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+                let pixel_offset = i * framebuffer.pitch() + i * 4;
+
+                // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
+                unsafe {
+                    framebuffer
+                        .addr()
+                        .add(pixel_offset as usize)
+                        .cast::<u32>()
+                        .write(0xFFFFFFFF)
+                };
+            }
+        }
+    }
+
+    hcf();
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
+    hcf();
+}
+
+fn hcf() -> ! {
+    loop {
+        unsafe {
+            #[cfg(target_arch = "x86_64")]
+            asm!("hlt");
+            #[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))]
+            asm!("wfi");
+            #[cfg(target_arch = "loongarch64")]
+            asm!("idle 0");
+        }
+    }
 }
